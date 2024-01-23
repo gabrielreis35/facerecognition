bcb9207cd60da01062dd7df8ad8edf667d2063d9
Gabriel Reis Ruela <gabrielreis@vixdynamics.com.br>
2024-01-23 14:45:08 -0300

    Finalização do reconhecimento facial

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ import face_recognition as fr
 import os
 
 preSetImages = []
+compareImages = []
 preSetFolder = os.listdir('./preset')
+compareFolder = os.listdir('./compare')
 
 for item in preSetFolder:
     preSetImages.append(item)
 
+for image in compareFolder:
+    compareImages.append(image)
+
 for item in preSetImages:
-    path = "./preset/" + str(item)
-    imageTrained = fr.load_image_file(path)
-    imageTrained = cv2.cvtColor(imageTrained, cv2.COLOR_BGR2RGB)
+    for image in compareImages:
+        pathImage = "./compare/" + str(image)
+        pathPreset = "./preset/" + str(item)
+        
+        compareImage = fr.load_image_file(pathImage)
+        compareImage = cv2.cvtColor(compareImage, cv2.COLOR_BGR2RGB)
+        
+        presetImage = fr.load_image_file(pathPreset)
+        presetImage = cv2.cvtColor(presetImage, cv2.COLOR_BGR2RGB)
     
-    faceLoc = fr.face_locations(imageTrained)[0]
+        faceLoc = fr.face_locations(compareImage)[0]
     
-    cv2.rectangle(imageTrained, (faceLoc[3], faceLoc[0], faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
-    encodeImage = fr.face_encodings(imageTrained)[0]
+        cv2.rectangle(compareImage, (faceLoc[3], faceLoc[0], faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
+        
+        encodePreset = fr.face_encodings(presetImage)[0]
+        encodeImage = fr.face_encodings(compareImage)[0]
+        
+        compare = fr.compare_faces([encodeImage], encodePreset)
+        
+        print(compare)
     
-    # compare = fr.compare_faces([encodeImage], encodeCompare)
-    
-    print(encodeImage)
-    cv2.imshow("teste", imageTrained)
+        # print(encodePreset)
+        cv2.imshow("teste", compareImage)
     cv2.waitKey(0)
     
 cv2.destroyAllWindows()
