a71ee1c69efd4806a4207bcdedac8b1fefeee9d2
Gabriel Reis Ruela <gabrielreis@vixdynamics.com.br>
2024-01-25 10:33:08 -0300

    Feito as alterações para verificação da imgem

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import tempfile
 import cv2
 import face_recognition as fr
 import os
 import time
+import threading
+import numpy as np
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
 
@@ -14,15 +18,14 @@ compareTime = time.time()
 
 def VerificaImagemPasta():
     presetFolder = os.listdir('./preset')
-    # if os.path.exists(presetFolder) == False:
-    #     return False
     for item in presetFolder:
         preSetImages.append(item)
+    print(len(preSetImages))
 
-for image in compareFolder:
-    compareImages.append(image)
+# for image in compareFolder:
+#     compareImages.append(image)
 
-VerificaImagemPasta()
+threading.Thread(target=VerificaImagemPasta).start()
 
 while (cap.isOpened()):
     ret, frame = cap.read()
@@ -32,32 +35,42 @@ while (cap.isOpened()):
     
     now = time.time()
     if now - compareTime > 300:
-        VerificaImagemPasta()
+        threading.Thread(target=VerificaImagemPasta).start()
+    
+    frame_array = np.array(frame)
+    
+    img = Image.fromarray(cv2.cvtColor(frame_array, cv2.COLOR_BGR2RGB))
+    temp_file_path = os.path.join(tempfile.gettempdir(), "temp_frame.jpg")
+    img.save(temp_file_path)
+    
+    compareFrame = fr.load_image_file(temp_file_path)
+    os.remove(temp_file_path)
+    compareFrame = cv2.cvtColor(compareFrame, cv2.COLOR_BGR2RGB)
+    
+    if len(fr.face_locations(compareFrame)) > 0:
+        faceLoc = fr.face_locations(compareFrame)[0]
+        cv2.rectangle(compareFrame, (faceLoc[3], faceLoc[0], faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
+    
+    for image in preSetImages:
+        pathImage = "./preset/" + str(image)
         
-    for image in compareImages:
-        pathImage = "./compare/" + str(image)
-        
-        compareImage = fr.load_image_file(pathImage)
-        compareImage = cv2.cvtColor(compareImage, cv2.COLOR_BGR2RGB)
-        
-        presetImage = fr.load_image_file(frame)
+        presetImage = fr.load_image_file(pathImage)
         presetImage = cv2.cvtColor(presetImage, cv2.COLOR_BGR2RGB)
-    
-        faceLoc = fr.face_locations(compareImage)[0]
-    
-        cv2.rectangle(compareImage, (faceLoc[3], faceLoc[0], faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
         
         encodePreset = fr.face_encodings(presetImage)[0]
-        encodeImage = fr.face_encodings(compareImage)[0]
         
-        compare = fr.compare_faces([encodeImage], encodePreset)
+        encodeImage = fr.face_encodings(compareFrame)
+        if encodeImage:
+            encodeImage = encodeImage[0]
+            compare = fr.compare_faces([encodeImage], encodePreset)
         
         print(compare)
-    
-        # print(encodePreset)
-        cv2.imshow("teste", compareImage)
-    cv2.waitKey(0)
-    
+        
+    cv2.imshow("teste", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
 
 
